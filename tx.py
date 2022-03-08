a4d3f7497445eb0c8cfa54916ebc00c559712a9e
tuananhtq44 <101157273+tuananhtq44@users.noreply.github.com>
2022-03-08 10:09:14 +0700

    Add files via upload

---
+++ tx.py
@@ -0,0 +1,93 @@
+<h1 align = "center"> Xin chào 👋, tôi là Tuấn Anh </h1>
+<p align = "center"> <img src = "https://img.icons8.com/color/48/000000/vietnam-circular.png" /> </p>
+<h3 align = "center"> Một nhà phát triển nhiệt huyết đến từ Việt Nam </h3>
+<p align = "center"> <img src = "https://komarev.com/ghpvc/?username=tiennhm" alt = "tiennhm" /> <img src = "https://badges.pufler.dev/ repos / TienNHM "alt =" tiennhm "/> </p>
+
+- ✍ Mình là sinh viên của: [Đại học Sư phạm Công nghệ Tp.Hà Nội] (https://hcmute.edu.vn) (HCMUTE).
+
+- 🌱 Tôi hiện đang học ** MERN Stack. **
+
+
+## 📫 Cách liên hệ với tôi:
+<p align = "center">
+  <a href="https://github.com/tuananhtq44" alt="Github">
+    <img src = "https://img.icons8.com/fluent/48/000000/github.png" />
+  </a>
+  <a href="https://www.facebook.com/NGUYENTUANANH.FACEBOOK.SUPPORT" alt="Facebook" target="_blank">
+    <img src = "https://img.icons8.com/fluent/48/000000/youtube-play.png" />
+  </a>
+
+
+## Kỹ năng:
+<p align = "center">
+  <img src = "https://www.vectorlogo.zone/logos/opencv/opencv-icon.svg" alt = "opencv" width = "48" height = "48" />
+  <img src = "https://img.icons8.com/color/48/000000/microsoft-sql-server.png" />
+  <img src = "https://img.icons8.com/color/48/000000/mysql-logo.png" />
+  <img src = "https://img.icons8.com/color/48/000000/mongodb.png" />
+  <img src = "https://img.icons8.com/fluent/48/000000/matlab.png" />
+  <img src = "https://img.icons8.com/color/48/000000/git.png" />
+  <img src = "https://img.icons8.com/color/48/000000/github-2.png" />
+  <img src = "https://img.icons8.com/color/48/000000/visual-studio-code-2019.png" />
+  <img src = "https://img.icons8.com/color/48/000000/visual-studio-2019.png" />
+  <img src = "https://img.icons8.com/dusk/48/000000/anaconda.png" />
+  <img src = "https://img.icons8.com/fluent/48/000000/spyder-ide.png" />
+  <img src = "https://img.icons8.com/color/48/000000/trello.png" />
+</p>
+
+<table style = "width: 100%;">
+  <tr>
+    <td>
+      <img src = "https://github-readme-stats.vercel.app/api/top-langs/?username=tiennhm&bg_color=FFFFFF00&text_color=179fa3&layout=compact&hide=CSS&langs_count=10&custom_title=Top%dùng%20đ alt = "tiennhm" width = "100%" />
+      <img src = "https://github-readme-stats.vercel.app/api?username=tiennhm&bg_color=FFFFFF00&text_color=179fa3&show_icons=true&count_private=true&include_all_comthubtrên=true&custom_ộng_tiêu=Honhm%20đ" width = " "100%" />
+    </td>
+    <td>
+      <p align = "center">
+        <img src = "https://cdn.dribbble.com/users/1059583/screenshots/4171367/coding-freak.gif" alt = "dev" width = "100%" />
+      </p>
+    </td>
+  </tr>
+</table>
+
+# Chứng chỉ:
+
+<p align = "center">
+  <a href="https://matlabacademy.mathworks.com/progress/share/certificate.html?id=c2f444b8-d6ce-4eef-9934-48d7fa7da2d1">
+    <img alt = "MATLAB Onramp" title = "MATLAB Onramp" src = "Certific / MATLAB-Onramp-certificate.png" width = "300px" />
+  </a>
+  <a href="https://matlabacademy.mathworks.com/progress/share/certificate.html?id=ad7fb8de-67d7-487f-95ee-f3871a61b1e1">
+    <img alt = "Machine Learning Onramp" title = "Machine Learning Onramp" src = "Certific / Machine-Learning-Onramp-certificate.png" width = "300px" />
+  </a>
+</p>
+
+<p align = "center">
+  <a href="https://www.coursera.org/account/accomplishments/certificate/XFNU3UXCK5DG">
+    <img alt = "Giới thiệu về JavaScript" title = "Giới thiệu về JavaScript" src = "certificate / Coursera% 20XFNU3UXCK5DG.png" width = "300px" />
+  </a>
+  <a href="https://www.coursera.org/account/accomplishments/certificate/MBSDFCKQ9X8E">
+    <img alt = "Phân loại âm thanh với TensorFlow" title = "Phân loại âm thanh với TensorFlow" src = "certificate / Coursera% 20MBSDFCKQ9X8E.png" width = "300px" />
+  </a>
+</p>
+
+<p align = "center">
+  <a href="https://www.coursera.org/account/accomplishments/certificate/PQMJRCLM7BCQ">
+    <img alt = "Cấu trúc dữ liệu Python" title = "Cấu trúc dữ liệu Python" src = "certificate / Coursera% 20PQMJRCLM7BCQ.png" width = "200px" />
+  </a>
+  <a href="https://www.coursera.org/account/accomplishments/certificate/V7MK7JDL96DU">
+    <img alt = "Lập trình cho mọi người (Bắt đầu với Python)" title = "Lập trình cho mọi người (Bắt đầu với Python)" src = "certificate / Coursera% 20V7MK7JDL96DU.png" width = "200px" />
+  </a>
+  <a href="https://www.coursera.org/account/accomplishments/certificate/DVXXD98ESKLP">
+    <img alt = "Capstone: Truy xuất, xử lý và hiển thị dữ liệu bằng Python" title = "Capstone: Truy xuất, xử lý và hiển thị dữ liệu bằng Python" src = "certificate / Coursera% 20DVXXD98ESKLP.png" width = "200px" />
+  </a>
+</p>
+
+<p align = "center">
+  <a href="https://www.kaggle.com/learn/certification/nguyenhuynhminhtien/python">
+    <img alt = "Kaggle - Python" title = "Kaggle - Python" src = "Certificates / Nguyen% 20Huynh% 20Minh% 20Tien% 20-% 20Python.png" width = "200px" />
+  </a>
+  <a href="https://www.kaggle.com/learn/certification/nguyenhuynhminhtien/intro-to-machine-learning">
+    <img alt = "Kaggle - Intro to Machine Learning" title = "Kaggle - Intro to Machine Learning" src = "certificate / Nguyen% 20Huynh% 20Minh% 20Tien% 20-% 20Intro% 20to% 20Machine% 20Learning.png" width = "200px" />
+  </a>
+  <a href="https://www.kaggle.com/learn/certification/nguyenhuynhminhtien/intro-to-deep-learning">
+    <img alt = "Kaggle - Intro to Deep Learning" title = "Kaggle - Intro to Deep Learning" src = "certificate / Nguyen% 20Huynh% 20Minh% 20Tien% 20-% 20Intro% 20to% 20Deep% 20Learning.png" width = "200px" />
+  </a>
+</p>
